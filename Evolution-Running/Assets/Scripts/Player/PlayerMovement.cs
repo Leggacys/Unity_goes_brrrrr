470cc4905b3496c7024934c88cb0cb4edc45efeb
Andrei <boogyman1989@yahoo.com>
2022-03-26 21:27:47 +0200

    added attack

---
+++ Evolution-Running/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+	#region Singleton
+
+	public static PlayerMovement instance;
+    
+	private void Awake()
+	{
+		if (instance == null)
+		{
+			instance = this;
+		} else if(instance!=this)
+		{
+			Destroy(gameObject);
+		}
+	}
+	
+
+	#endregion
+	
+	
+	public float gravity = -9.18f;
+	
+	public bool isGrounded { get; set; }
+	
+	private Vector3 velocity = new Vector3(2 , 0 ,0);
+	private Rigidbody rb;
+	
+
+	[Header("Attack")]
+	public GameObject projectile;
+	public float throwPower;
+	public bool canAttack;
+    public float timeBetweenAttacks;
+    
+	private Vector3 direction;
+	public LayerMask ground;
+	public float raycastDistance;
+
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	private void FixedUpdate()
+	{
+		rb.MovePosition(  rb.position + new Vector3(0,velocity.y,
+			velocity.z* PlayerManager.instace.speed) * Time.fixedDeltaTime);
+		Gravity();
+		TakeGroundAngle();
+	}
+	
+	private void OnCollisionEnter(Collision collision)
+	{
+		PlayerAnimations.instance.FallAnimation();
+		isGrounded = true;
+	}
+	
+	private void Gravity()
+	{
+		if (isGrounded && velocity.y < 0)
+		{
+			velocity.y = -2f;
+		}
+
+		velocity.y += gravity * Time.deltaTime;
+	}
+	
+	
+	public void Jump()
+	{
+		isGrounded = false;	
+		velocity.y = Mathf.Sqrt(PlayerManager.instace.jumpAmount * -2f * gravity);
+		PlayerAnimations.instance.JumpEffect(PlayerManager.instace.playerBody);
+		
+	}
+
+	void TakeGroundAngle()
+	{
+		RaycastHit hit;
+
+		if (Physics.Raycast(transform.position, -transform.up, out hit,raycastDistance, ground))
+		{
+			Quaternion angle = Quaternion.FromToRotation(transform.up, hit.normal);
+			transform.rotation = Quaternion.Slerp(transform.rotation,angle * transform.rotation,10);
+		}
+	}
+
+
+	public void Attack()
+    {
+        GameObject newProjectile = Instantiate(projectile,transform.position + transform.forward + transform.up, Quaternion.identity);
+        newProjectile.GetComponent<Rigidbody>().AddForce((transform.up + transform.forward)*throwPower,ForceMode.Impulse);
+        StartCoroutine(attackCooldown());
+
+    }
+
+    IEnumerator attackCooldown(){
+        yield return new WaitForSeconds(timeBetweenAttacks);
+        canAttack = true;
+    }
+}
